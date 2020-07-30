7a71cd1611e41f07bc21f3ffd69f5247c0ab0e66
Kimginam97 <65409092+Kimginam97@users.noreply.github.com>
2020-07-30 17:27:20 +0900

    Game:Final touch

--- 게임만들기/main.js
+++ 게임만들기/main.js
@@ -15,6 +15,12 @@ const popUp=document.querySelector('.pop-up');
 const popUpText=document.querySelector('.pop-up__message');
 const popUpRefresh=document.querySelector('.pop-up__refresh');
 
+const carrotSound=new Audio('./sound/carrot_pull.mp3');
+const alertSound=new Audio('./sound/alert.wav');
+const bgSound=new Audio('./sound/bg.mp3');
+const bugSound=new Audio('./sound/bug_pull.mp3');
+const winSound=new Audio('./sound/game_win.mp3');
+
 let started=false;
 let score=0;
 let timer=undefined;
@@ -40,16 +46,28 @@ function startGame(){
     showStopButton();
     showTimerAndScore();
     startGameTimer();
+    playSound(bgSound);
 }
 
 function stopGame(){
     started=false;
     stopGameTimer();
+    hideGameButton();
+    showPopUpWithText('REPLAY?');
+    playSound(alertSound);
+    stopSound(bgSound);
 }
 
 function finishGame(win){
     started=false;
     hideGameButton();
+    if(win){
+        playSound(winSound);
+    }else{
+        playSound(bugSound);
+    }
+    stopGameTimer();
+    stopSound(bgSound);
     showPopUpWithText(win?'YOU WON!' : 'YOU LOST');
 }
 
@@ -62,14 +80,13 @@ function startGameTimer(){
             finishGame(CARROT_COUNT===score);
             return;
         }
-        updateTimerText(--remainingTimeSec)
+        updateTimerText(--remainingTimeSec);
     },1000)
 }
 
 function stopGameTimer(){
     clearInterval(timer);
-    hideGameButton();
-    showPopUpWithText('REPLAY?');
+    
 }
 
 function updateTimerText(time){
@@ -87,6 +104,7 @@ function showStopButton(){
     const icon=gameBtn.querySelector('.fas');
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+    gameBtn.style.visibility='visible';
 }
 
 function hideGameButton(){
@@ -103,6 +121,7 @@ function hidePopUp(){
 }
 
 function initGame(){
+    score=0;
     field.innerHTML='';
     gameScore.innerText=CARROT_COUNT;
     // 벌레와 당근을 생성한뒤 field에 추가해줌
@@ -119,17 +138,25 @@ function onFiledClick(event){
         // 당근
         target.remove();
         score++;
+        playSound(carrotSound);
         updateScoreBoard();
         if(score===CARROT_COUNT){
             finishGame(true);
         }
     }else if(target.matches('.bug')){
         // 벌레
-        stopGameTimer();
         finishGame(false);
     }
 }
 
+function playSound(sound){
+    sound.currentTime=0;
+    sound.play();
+}
+
+function stopSound(sound){
+    sound.pause();
+}
 
 function updateScoreBoard(){
     gameScore.innerText=CARROT_COUNT-score;
