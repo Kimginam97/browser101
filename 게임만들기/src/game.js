0991902aaee194a71ed13614cd26d802422d3456
Kimginam97 <65409092+Kimginam97@users.noreply.github.com>
2020-07-31 16:43:07 +0900

    Game:빌더패턴

--- 게임만들기/src/game.js
+++ 게임만들기/src/game.js
@@ -3,7 +3,35 @@
 import * as sound from './sound.js';
 import Field from './field.js';
 
-export default class Game{
+// Builder Pattern
+export default class GameBuilder{
+    gameDuration(duration){
+        this.gameDuration=duration;
+        return this;
+    }
+
+    carrotcount(num){
+        this.carrotcount=num;
+        return this;
+    }
+
+    bugCount(num){
+        this.bugCount=num;
+        return this;
+    }
+
+    build(){
+        return new Game(
+            this.gameDuration,
+            this.carrotcount,
+            this.bugCount
+        );
+            
+        
+    }
+}
+
+class Game{
     constructor(gameDuration,carrotCount,bugCount){
         this.gameDuration=gameDuration;
         this.carrotCount=carrotCount;
