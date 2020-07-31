89096751716860cd05f315a55f89a041fd794286
Kimginam97 <65409092+Kimginam97@users.noreply.github.com>
2020-07-31 13:35:30 +0900

    Game:Final touch

--- 게임만들기/main.js
+++ 게임만들기/main.js
@@ -1,18 +1,19 @@
 'use strict'
-import PopUp from './popup.js';
-import Field from './field.js';
 
-
+const CARROT_SIZE=80;
 const CARROT_COUNT=5;
 const BUG_COUNT=5;
 const GAME_DURATION_SEC=5;
 
-
+const field = document.querySelector('.game__field');
+const fieldRect=field.getBoundingClientRect();
 const gameBtn=document.querySelector('.game__button');
 const gameTimer=document.querySelector('.game__timer');
 const gameScore=document.querySelector('.game__score');
 
-
+const popUp=document.querySelector('.pop-up');
+const popUpText=document.querySelector('.pop-up__message');
+const popUpRefresh=document.querySelector('.pop-up__refresh');
 
 const carrotSound=new Audio('./sound/carrot_pull.mp3');
 const alertSound=new Audio('./sound/alert.wav');
@@ -24,28 +25,7 @@ let started=false;
 let score=0;
 let timer=undefined;
 
-const gameFinishBanner=new PopUp();
-gameFinishBanner.setClickListener(()=>{
-    startGame();
-});
-
-const gamefield=new Field(CARROT_COUNT,BUG_COUNT);
-gamefield.setClickListener(onItemClick);
-
-function onItemClick(item){
-    if(!started){
-        return;
-    }
-    if(item==='carrot'){
-        score++;
-        updateScoreBoard();
-        if(score===CARROT_COUNT){
-            finishGame(true);
-        }
-    }else if(item==='bug'){
-        finishGame(false);
-    }
-}
+field.addEventListener('click',(event)=>onFiledClick(event));
 
 gameBtn.addEventListener('click',()=>{
     if(started){
@@ -53,6 +33,11 @@ gameBtn.addEventListener('click',()=>{
     }else{
         startGame();
     }
+})
+
+popUpRefresh.addEventListener('click',()=>{
+    startGame();
+    hidePopUp();
 })
 
 function startGame(){
@@ -68,7 +53,7 @@ function stopGame(){
     started=false;
     stopGameTimer();
     hideGameButton();
-    gameFinishBanner.showWithText('REPLAY?');
+    showPopUpWithText('REPLAY?');
     playSound(alertSound);
     stopSound(bgSound);
 }
@@ -83,7 +68,7 @@ function finishGame(win){
     }
     stopGameTimer();
     stopSound(bgSound);
-    gameFinishBanner.showWithText(win?'YOU WON!' : 'YOU LOST');
+    showPopUpWithText(win?'YOU WON!' : 'YOU LOST');
 }
 
 function startGameTimer(){
@@ -126,14 +111,43 @@ function hideGameButton(){
     gameBtn.style.visibility='hidden';
 }
 
+function showPopUpWithText(text){
+    popUpText.innerText=text;
+    popUp.classList.remove('pop-up--hide');
+}
+
+function hidePopUp(){
+    popUp.classList.add('pop-up--hide');
+}
 
 function initGame(){
     score=0;
+    field.innerHTML='';
     gameScore.innerText=CARROT_COUNT;
-    gamefield.init();
+    // 벌레와 당근을 생성한뒤 field에 추가해줌
+    addItem('carrot',CARROT_COUNT,'img/carrot.png');
+    addItem('bug',BUG_COUNT,'img/bug.png');
 }
 
-
+function onFiledClick(event){
+    if(!started){
+        return;
+    }
+    const target=event.target;
+    if(target.matches('.carrot')){
+        // 당근
+        target.remove();
+        score++;
+        playSound(carrotSound);
+        updateScoreBoard();
+        if(score===CARROT_COUNT){
+            finishGame(true);
+        }
+    }else if(target.matches('.bug')){
+        // 벌레
+        finishGame(false);
+    }
+}
 
 function playSound(sound){
     sound.currentTime=0;
@@ -146,5 +160,27 @@ function stopSound(sound){
 
 function updateScoreBoard(){
     gameScore.innerText=CARROT_COUNT-score;
+}
+
+function addItem(className,count,imgPath){
+    const x1=0;
+    const y1=0;
+    const x2=fieldRect.width - CARROT_SIZE;
+    const y2=fieldRect.height - CARROT_SIZE;
+    for(let i=0;i<count;i++){
+        const item=document.createElement('img');
+        item.setAttribute('class',className);
+        item.setAttribute('src',imgPath);
+        item.style.position='absolute';
+        const x=randomNumber(x1,x2);
+        const y=randomNumber(y1,y2);
+        item.style.left=`${x}px`;
+        item.style.top=`${y}px`;
+        field.appendChild(item);
+    }
+}
+
+function randomNumber(min,max){
+    return Math.random()*(max-min)+min;
 }
 
