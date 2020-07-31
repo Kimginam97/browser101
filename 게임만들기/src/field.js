d8fdaf6fef3ba7339644aad56d738ad1da6a406a
Kimginam97 <65409092+Kimginam97@users.noreply.github.com>
2020-07-31 15:21:23 +0900

    Game:Sound.js

--- 게임만들기/src/field.js
+++ 게임만들기/src/field.js
@@ -1,6 +1,6 @@
 'use strict'
 const CARROT_SIZE=80;
-const carrotSound=new Audio('./sound/carrot_pull.mp3');
+import * as sound from './sound.js';
 export default class Field{
     constructor(carrotCount,bugCount){
         this.carrotCount=carrotCount;
@@ -41,7 +41,7 @@ export default class Field{
         const target=event.target;
     if(target.matches('.carrot')){
         target.remove();
-        playSound(carrotSound);
+        sound.playCarrot();
         this.onItemClick && this.onItemClick('carrot');
     }else if(target.matches('.bug')){
         this.onItemClick && this.onItemClick('bug');
@@ -52,10 +52,6 @@ function randomNumber(min,max){
     return Math.random()*(max-min)+min;
 }
 
-function playSound(sound){
-    sound.currentTime=0;
-    sound.play();
-}
 
 
 
