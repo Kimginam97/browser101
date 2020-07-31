e89d07cb98da54670a1feb135b3b5a5a53496f41
Kimginam97 <65409092+Kimginam97@users.noreply.github.com>
2020-07-31 17:11:43 +0900

    Game:함수정리

--- 게임만들기/src/game.js
+++ 게임만들기/src/game.js
@@ -49,7 +49,7 @@ class Game{
 
         this.gameBtn.addEventListener('click',()=>{
             if(this.started){
-                this.stop();
+                this.stop(Reason.cancel);
             }else{
                 this.start();
             }
@@ -77,28 +77,12 @@ class Game{
         sound.playBackground();
     }
 
-    stop(){
+    stop(reason){
         this.started=false;
         this.stopGameTimer();
         this.hideGameButton();
-        sound.playAlert();
         sound.stopBackground();
-        this.onGameStop && this.onGameStop(Reason.cancel);
-    }
-
-    
-
-    finish(win){
-    this.started=false;
-    this.hideGameButton();
-        if(win){
-          sound.playWin();
-        }else{
-         sound.playBug();
-        }
-        this.stopGameTimer();
-        sound.stopBackground();
-        this.onGameStop && this.onGameStop(win ? Reason.win : Reason.lose);
+        this.onGameStop && this.onGameStop(reason);
     }
 
     onItemClick = item =>{
@@ -109,10 +93,10 @@ class Game{
             this.score++;
             this.updateScoreBoard();
             if(this.score===this.carrotCount){
-                this.finish(true);
+                this.stop(Reason.win);
             }
         }else if(item==='bug'){
-            this.finish(false);
+            this.stop(Reason.lose);
         }
     }
 
@@ -122,7 +106,7 @@ class Game{
         this.timer=setInterval(()=>{
             if(remainingTimeSec<=0){
                 clearInterval(this.timer);
-                this.finish(this.carrotCount===this.score);
+                this.stop(this.carrotCount===this.score?Reason.win : Reason.lose);
                 return;
             }
             this.updateTimerText(--remainingTimeSec);
