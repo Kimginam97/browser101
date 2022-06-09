1b0f3ead1e1496da3d44f76311fa8c40a85176a6
Kimginam97 <rlarlska79@naver.com>
2022-06-10 08:23:11 +0900

    keydown -> form submit

--- DOM완전정복실전/main.js
+++ DOM완전정복실전/main.js
@@ -1,6 +1,12 @@
 const items=document.querySelector('.items');
+const form = document.querySelector('.new-form');
 const input=document.querySelector('.footer__input');
 const addBtn=document.querySelector('.footer__button');
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    onAdd();
+});
 
 function onAdd(){
     // 1.사용자가 입력한 텍스트를 받아온다
@@ -35,52 +41,13 @@ function createItem(text){
         <div class="item__divider"></div>
     `;
 
-
-    // const item=document.createElement('div');
-    // item.setAttribute('class','item');
-
-    // const name=document.createElement('span');
-    // name.setAttribute('class','item__name');
-    // name.innerText=text;
-
-    // const deleteBtn=document.createElement('button');
-    // deleteBtn.setAttribute('class','item__delete');
-    // deleteBtn.innerHTML='<i class="fas fa-trash-alt"></i>';
-    // deleteBtn.addEventListener('click',()=>{
-    //     items.removeChild(itemRow);
-    // })
-
-    // const itemDivider=document.createElement('div');
-    // itemDivider.setAttribute('class','item__divider');
-
-    // item.appendChild(name);
-    // item.appendChild(deleteBtn);
-
-    // itemRow.appendChild(item);
-    // itemRow.appendChild(itemDivider);
     id++;
     return itemRow;
 }
 
-addBtn.addEventListener('click',()=>{
-    onAdd();
-});
-
-input.addEventListener('keydown',(event)=>{
-    if(event.isComposing){
-        return;
-    }
-
-    if(event.key==='Enter'){
-        onAdd();
-    }
-})
-
 items.addEventListener('click',event=>{
     const id=event.target.dataset.id;
-    // if(event.target.nodeName==='I'){
-    //     console.log('he');
-    // }
+
     if(id){
         const toBeDeleted=document.querySelector(`.item__row[data-id="${id}"]`);
         toBeDeleted.remove();
